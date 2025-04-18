77d0c8f21c8ecfae59287b1334d7d0a5f2f298f1
Thomas Galarneau <114345819+Hamiltonius@users.noreply.github.com>
2025-04-17 19:44:35 -0700

    Update regulus_scraper.py
    triggered a side quest to new Regulus repo

--- regulus_scraper.py
+++ regulus_scraper.py
@@ -1,16 +1,10 @@
-import webbrowser
-import sys
+"""
+You've triggered a side quest!
 
-def main():
-    print("\nThis script (regulus_scraper.py) has been archived.")
-    print("For the latest stable version of Regulus, use regulus.py at the root of this repo.")
-    print("Opening the latest file in your browser now...\n")
-    webbrowser.open("https://github.com/Hamiltonius/Regulus/blob/main/regulus.py")
-    sys.exit(0)
+The old Regulus project has retired.
 
-if __name__ == "__main__":
-    main()
+To continue your journey, venture here:
+https://github.com/Hamiltonius/Regulus
 
-
-
-
+Good luck, traveler.
+"""
